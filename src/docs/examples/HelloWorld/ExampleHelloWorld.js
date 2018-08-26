8b5b8e51cb0ad89128cd016273b4c0b54429150c
Jose Obed Moguel Ruiz <jo_obed@hotmail.com>
2018-08-26 14:46:39 -0500

    Adding test.

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,8 @@
+
+import React from 'react';
+import HelloWorld from 'ps-react/HelloWorld';
+
+/** Custom message */
+export default function ExampleHelloWorld() {
+  return <HelloWorld message="Pluralsight viewers!" />
+}
---
+++ src/utils/percentUtils.js
@@ -0,0 +1,5 @@
+
+// Returns the width of a percent of total width, as an integer
+export function getWidthAsPercentOfTotalWidth(percent, totalWidth) {
+    return parseInt(totalWidth * (percent / 100), 10);
+  }
