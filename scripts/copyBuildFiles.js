5056b7d3b3ddb5117d9abd970ca42ff3c2d4dcb2
Jose Obed Moguel Ruiz <jo_obed@hotmail.com>
2018-08-28 00:23:44 -0500

    adding configuration

--- scripts/copyBuildFiles.js
+++ scripts/copyBuildFiles.js
@@ -56,7 +56,7 @@ function createPackageFile() {
     } = packageData;
 
     const minimalPackage = {
-      name: 'ps-react',
+      name: 'ps-react-jmoguelruiz',
       author,
       version,
       description,
